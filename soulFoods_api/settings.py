4e9592bc4a1814a754a43ddf4ead5e1672a798c5
Vaibhav Doddihal <vibbsdod@Vaibhavs-MacBook-Pro.local>
2018-10-24 19:06:06 +0530

    bootstrapping the application with custom styling along with django-bootstrap

--- soulFoods_api/settings.py
+++ soulFoods_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'soulFoodApp'
+    'soulFoodApp',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
